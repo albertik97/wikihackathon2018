8c5e8f263caeeb1c0ca22ce9c5af1514166cd7ca
Unknown <alberto.berenguer.97@gmail.com>
2018-10-18 12:38:44 +0200

    Home creado

---
+++ weelink/src/home/home.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+//import {Location} from '../services/location.sercive';
+
+@Component({
+	selector:'home',
+	templateUrl:'./home.component.html'
+})
+
+export class HomeComponent{
+	public titulo = "Home";
+}
---
+++ weelink/src/services/location.sercive.ts
@@ -0,0 +1,31 @@
+import {Injectable} from '@angular/core';
+
+@Injectable()
+
+/*var options = {
+		  enableHighAccuracy: true,
+		  timeout: 6000,
+		  maximumAge: 0
+		};
+
+export class LocationService{
+		public position;
+
+		
+
+		localizar(){
+			navigator.geolocation.getCurrentPosition(this.success, this.error, options );
+	    }
+		success(position) {
+		  var coordenadas = position.coords;
+
+		  console.log('Tu posición actual es:');
+		  console.log('Latitud : ' + coordenadas.latitude);
+		  console.log('Longitud: ' + coordenadas.longitude);
+		  console.log('Más o menos ' + coordenadas.accuracy + ' metros.');
+		};
+
+		error(error) {
+		  console.warn('ERROR(' + error.code + '): ' + error.message);
+		};
+}*/
