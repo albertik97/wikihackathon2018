915aa8a4c725b96cce4271f1448b71ac96359fcf
Unknown <alberto.berenguer.97@gmail.com>
2018-10-19 12:21:54 +0200

    vidas

--- weelink/src/pregunta/pregunta.component.ts
+++ weelink/src/pregunta/pregunta.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {PeticionesService} from '../services/peticiones.service';
 
+
 @Component({
 	selector:'pregunta',
 	templateUrl:'./pregunta.component.html',
@@ -12,10 +13,14 @@ import {PeticionesService} from '../services/peticiones.service';
 export class PreguntaComponent{
 	public pregunta = "quiz";
 	public correcta = "";
-	public contestada:boolean;
+	public acertada:string;
 	public url_imagen;
 	public p1;
 	public n1;
+	public p1_color;
+	public p2_color;
+	public p3_color;
+	public p4_color;
 	public p2;
 	public n2;
 	public p3;
@@ -25,15 +30,20 @@ export class PreguntaComponent{
 	public puntuacion;
 	public number;
 	public urlCategoria1; //monumentos
+	public vida;
 
 	constructor(private _PeticionesService:PeticionesService){
 		this.url_imagen="";
 	}
 	ngOnInit(){
-		
+		this.vida=3;
+		this.p1_color="white";
+		this.p2_color="white";
+		this.p3_color="white";
+		this.p4_color="white";
 		this.number=-1;
 		this.puntuacion=0;
-		this.contestada=false;
+		this.acertada="0";//no contestada
 		this.urlCategoria1=`SELECT DISTINCT ?mon ?monLabel ?esta_enLabel ?foto WHERE {
 					 ?mon wdt:P131* wd:Q54936.
 					 ?mon wdt:P31 wd:Q4989906.
@@ -43,16 +53,21 @@ export class PreguntaComponent{
 
 					}
 					LIMIT 1000`;
+
 		this.dothings();
 	}
 
  dothings(){
+ 		this.p1_color="white";
+		this.p2_color="white";
+		this.p3_color="white";
+		this.p4_color="white";
 
  	this._PeticionesService.getTipo1(this.urlCategoria1).subscribe(
             result => {
                  
                 if(result.code != 200){
-                    console.log(result.results.bindings);
+                    
                     var sitios = [];
                     this.number = 0;
                     this.number = Math.floor((Math.random() * result.results.bindings.length) + 1);
@@ -63,7 +78,7 @@ export class PreguntaComponent{
 				    sitios.push(number3);
 				    var number4 = Math.floor((Math.random() * result.results.bindings.length) + 1);
 				    sitios.push(number4);
-				    console.log(result.results.bindings[this.number]);
+
 				    this.url_imagen=result.results.bindings[this.number].foto.value;
 					sitios = this.shuffle(sitios);
 				    this.p1=result.results.bindings[sitios[0]].monLabel.value;
@@ -74,9 +89,8 @@ export class PreguntaComponent{
 				    this.n3=sitios[2];
 				    this.p4=result.results.bindings[sitios[3]].monLabel.value;
 				    this.n4=sitios[3];
-
                 }else{
-                    console.log(result.results.bindings);
+                   
                 }
  
             },
@@ -106,17 +120,64 @@ export class PreguntaComponent{
 	  return array;
 	}
 
-	comprobar(numb){
-		console.log(this.number);
+	comprobar(numb,n){
+		this.comprobar2(numb,n);
 		if(numb==this.number){
 			this.puntuacion+=100;
-			this.contestada=true;
-			this.dothings();
-			this.contestada=false;
-			this.number=-1;
+			this.acertada="1";
 		}else{
-			this.puntuacion-=11;
+			this.vida-=1;
+			this.acertada="2";
+			if(this.vida==0)
+				window.location.href = '/home';
 		}
+			this.dothings();
+			this.number=-1;
 	}
+
+	sleep(milliseconds) {
+	  var start = new Date().getTime();
+	  for (var i = 0; i < 1e7; i++) {
+	    if ((new Date().getTime() - start) > milliseconds){
+	      break;
+	    }
+	  }
+	}
+
+	comprobar2(n,num){
+		console.log(n);
+		console.log(num);
+		if(n==this.number&&num==1){
+			this.p1_color='green';
+			
+		}else{
+			this.p1_color='red';
+			console.log(this.p1_color);
+		}
+
+		if(n==this.number&&num==2){
+			this.p2_color='green';
+			
+		}else{
+			this.p2_color='red';
+		}
+
+		if(n==this.number&&num==3){
+			this.p3_color='green';
+			
+		}else{
+			this.p3_color='red';
+		}
+
+		if(n==this.number&&num==4){
+			this.p4_color='green';
+			
+		}else{
+			this.p4_color='red';
+		}
+			
+	}
+
+
 
 }
